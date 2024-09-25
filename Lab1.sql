bbf0468cae1bc759f48f10859b5e69508dea8ddb
RustamKhalimov <157012050+RustamKhalimov@users.noreply.github.com>
2024-09-25 14:21:29 +0600

    Create Lab1.sql

---
+++ Lab1.sql
@@ -0,0 +1,18 @@
+create database lab_1;
+create table usersss(
+    id int ,
+    firstname varchar(50),
+    lastname varchar(50),
+    primary key (id)
+);
+alter table usersss add isAdmin integer ;
+alter table usersss alter column isAdmin type boolean using (false);
+create table tasks(
+    id int,
+    name varchar(50),
+    user_id int,
+    primary key(id)
+);
+
+drop table tasks;
+drop database lab_1;
